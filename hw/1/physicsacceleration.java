d3b1915debfd02c0d680ad17f46b91fe85070b24
fauziah221 <70236530+fauziah221@users.noreply.github.com>
2020-09-04 16:14:27 -0400

    Update physicsacceleration.java

--- hw/1/physicsacceleration.java
+++ hw/1/physicsacceleration.java
@@ -4,18 +4,15 @@
 
  public class physicsacceleration {
 		    public static void main(String[] args) { 
-		    	double StartVelocity, EndVelocity, time; //variables
-		    	try (Scanner input = new Scanner(System.in)) {
+		    	double StartVelocity, EndVelocity, time; 
+		        try(Scanner input = new Scanner(System.in)) {
 					System.out.println("Type initial velocity(m/s): ");
-					StartVelocity = input.nextDouble(); //input initialVelocity
-					
+					StartVelocity = input.nextDouble(); 					
 					System.out.println("Type Type final velocity(m/s): ");
-					EndVelocity = input.nextDouble(); //input finalVelocity
-					
+					EndVelocity = input.nextDouble(); 					
 					System.out.println("Type the time in sec(s): ");
-					time = input.nextDouble(); //input time
+					time = input.nextDouble(); 
 				}
-
 		    	double result = (EndVelocity - StartVelocity)/time; //calculate average velocity
 		    	System.out.println("\n The average acceleration is: " +result);
 		    }
