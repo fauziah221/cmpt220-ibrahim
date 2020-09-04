cd7d82511aeb9e07e7d652003df71c7789f603ee
fauziah221 <70236530+fauziah221@users.noreply.github.com>
2020-09-04 16:10:43 -0400

    Update simplecomputation.java

--- hw/1/simplecomputation.java
+++ hw/1/simplecomputation.java
@@ -5,19 +5,17 @@
  public class simplecomputation {
 
 		    public static void main(String[] args) {   	
-		    	double a,b,c;
-		    	
+		    	double a,b,c;	
 		    	try (Scanner keyboard = new Scanner(System.in)) {
-					System.out.println("Type a number for a: ");
-					a = keyboard.nextDouble();
-					
-					System.out.println("Type a number for b: ");
+					System.out.println("Type a number for a= ");
+					a = keyboard.nextDouble();			
+					System.out.println("Type a number for b= ");
 					b = keyboard.nextDouble();
-					
-					System.out.println("Type a number for c: ");
+					System.out.println("Type a number for c:= ");
 					c = keyboard.nextDouble();
 				}
-		    	double discriminant = (b * b) - (4* a *c);
-		    	System.out.println("The quadratic equation's discriminant is" +discriminant); //finds the discriminant
+		    	double discriminant = (b * b) - (4* a *c); 
+		    	System.out.println("The quadratic equation's discriminant is =" +discriminant); //finds the discriminant
 		    }		    	  	
- }
+}
+
