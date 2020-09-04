c2f10bfc1af2f3c66c07a0d326e2aa4aecbf04b4
fauziah221 <70236530+fauziah221@users.noreply.github.com>
2020-09-04 16:20:51 -0400

    Problem 3

---
+++ hw/1/displaypattern.java
@@ -0,0 +1,16 @@
+ package cmpt220;
+
+ public class displaypattern {
+	 public static void main(String[] args) 
+     {
+			 //Printing to form a big 100 sign
+			 System.out.println("* ***** *****");
+			 System.out.println("* *   * *   *");
+			 System.out.println("* *   * *   *");
+			 System.out.println("* *   * *   *");
+			 System.out.println("* ***** *****");
+			 
+	 }
+ }
+
+
