f5a0ebc3f05efb68de235ba7ce0162ecbd28c88d
fauziah221 <70236530+fauziah221@users.noreply.github.com>
2020-09-18 21:01:32 -0400

    Problem 1

---
+++ hw/2/rectangle.java
@@ -0,0 +1,20 @@
+package cmpt220;
+
+import java.util.Scanner;
+
+public class rectangle {
+	public static void main(String[] args) {
+		
+           //Calculating the rectangle perimeter,area and diagonal length
+		Scanner rectangle = new Scanner(System.in);
+			System.out.println("Please type both the width and height of a rectangle:");
+			
+			double a= rectangle.nextDouble();
+			double b = rectangle.nextDouble();
+			
+			System.out.println("The periemter of the rectangle is: " + 2 * (a + b));// calculates the perimeter
+			System.out.println("The area of the rectangle is: " + a * b);//calculates the area 
+			System.out.println("The length of the diagonal in the rectangle is: "+Math.sqrt(a * a+ b * b));// calculates the diagonal length 
+		}
+}
+
