879a87a6ebf84ea23d11f491b98160ec209ba8c4
fauziah221 <70236530+fauziah221@users.noreply.github.com>
2020-10-23 05:14:36 -0400

    Add files via upload
    problem2

---
+++ hw/6/problem2.java
@@ -0,0 +1,23 @@
+package cmpt220;
+
+import java.util.Scanner;
+
+public class problem2 {
+		 public static int[] count(String s) {
+		      int[] count = new int[10];
+		      for(int i = 0; i < s.length(); ++i) {		        	
+		      if(Character.isDigit(s.charAt(i))) {
+		      count[s.charAt(i)-'0']++;}}
+		  return count;}
+
+		 public static void main(String[] args) {
+		      try (Scanner in = new Scanner(System.in)) {
+			   System.out.println("Enter a string: ");
+			   String l = in.nextLine();
+			   int[] count = count(l);
+			   for(int i = 0; i < count.length; ++i) {
+			   if(count[i] != 0) {
+			   System.out.printf("Digit %d occurs %d times\n", i, count[i]);
+	}}}}
+
+}
