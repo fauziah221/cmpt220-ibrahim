31904513a2349f3612c3ae9027798c85f8f0a0b0
fauziah221 <70236530+fauziah221@users.noreply.github.com>
2020-10-30 15:02:37 -0400

    Problem1
    The point program has a point class that would be called into the problem1 class in order for the code to run so please open both programs.

---
+++ hw/7/Point.java
@@ -0,0 +1,17 @@
+package cmpt220;
+
+public class Point {
+	double x,y; 
+    
+    public Point(double x, double y)
+    {this.x = x; 
+     this.y = y; 
+    } 
+    
+    static void answerForPoint(Point p)  
+    { 
+            System.out.println("(" + p.x + ", " + p.y + ")");} 
+} 
+
+
+
