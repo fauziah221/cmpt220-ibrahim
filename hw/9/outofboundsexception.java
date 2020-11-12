f4d37cea103b7030580b50654eb5c7428866b870
fauziah221 <70236530+fauziah221@users.noreply.github.com>
2020-11-12 03:32:06 -0500

    problem 1

---
+++ hw/9/outofboundsexception.java
@@ -0,0 +1,27 @@
+package cmpt220;
+
+import java.util.Random;
+
+import java.util.Scanner;
+
+public class outofboundsexception {
+	public static void main(String[] args) {
+		int n = 100;
+		
+		Random r = new Random();
+		Scanner outofboundsexception = new Scanner(System.in);
+		int[] array = new int[n];
+		 
+		for (int i = 0; i<n; i++) {
+			array[i] = 1 + r.nextInt(100);}
+		
+		System.out.println("Please enter the index of the array: ");
+		int x = outofboundsexception.nextInt();
+		
+	    try
+	    {int number = array[x];
+	    System.out.println("Corresponding element value is " + number);}
+	    catch(ArrayIndexOutOfBoundsException e) {
+	    	System.out.println("Out of bounds");}					
+	    }
+}
