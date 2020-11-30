94ff13b49460aece0237ea966efb9712c5ff475a
Juan Arias <juan.arias@marist.edu>
2020-11-29 20:48:59 -0500

    Feedback

--- hw/8/binpacking.java
+++ hw/8/binpacking.java
@@ -3,37 +3,35 @@
 import java.util.Scanner;
 
 public class binpacking {
-		public static int nextBin( int numberofobjects, int weightofobjects[], int containers) {
-				
-				int number = 1, content = containers;
-				
-				for (int i = 0; i<numberofobjects; i++) {
-					  if (weightofobjects[i] > content) { 
-		                  number++;
-		                  content = containers - weightofobjects[i]; 
-		          } 
-					  else
-		                  content -= weightofobjects[i]; 
-					  if((weightofobjects[i]+content)==10){
-		                  System.out.println("Container "+ number +" contains objects with weight "+ weightofobjects[i]+" "+content);}} 
-		  return number; 
-		} 
+	public static int nextBin(int numberofobjects, int weightofobjects[], int containers) {
 
-		public static void main(String[] args) { 
+		int number = 1, content = containers;
 
-			int containers = 10; 
-			System.out.println("Please enter the number of objects: ");
+		for (int i = 0; i < numberofobjects; i++) {
+			if (weightofobjects[i] > content) {
+				number++;
+				content = containers - weightofobjects[i];
+			} else
+				content -= weightofobjects[i];
+			if ((weightofobjects[i] + content) == 10) {
+				System.out.println(
+						"Container " + number + " contains objects with weight " + weightofobjects[i] + " " + content);
+			}
+		}
+		return number;
+	}
+
+	public static void main(String[] args) {
+
+		int containers = 10;
+		System.out.println("Please enter the number of objects: ");
 		Scanner bin = new Scanner(System.in);
 		int number = bin.nextInt();
 		System.out.println("Please enter the weights of the objects:");
-		int []obj = new int[number];
-		for(int i=0; i<number; i++)
+		int[] obj = new int[number];
+		for (int i = 0; i < number; i++)
 			obj[i] = bin.nextInt();
 
-	  
-				System.out.println("Number of bins required : " + nextBin(number, obj, containers)); 
-			}
+		System.out.println("Number of bins required : " + nextBin(number, obj, containers));
+	}
 }
-
-
-
--- hw/8/splitastring.java
+++ hw/8/splitastring.java
@@ -3,31 +3,34 @@
 import java.util.ArrayList;
 
 public class splitastring {
-	 public static ArrayList<String> split(String s, String delimiters){
-	        ArrayList<String> display = new ArrayList<String>();
-	        ArrayList<Character> d = new ArrayList<Character>();
+	public static ArrayList<String> split(String s, String delimiters) {
+		ArrayList<String> display = new ArrayList<String>();
+		ArrayList<Character> d = new ArrayList<Character>();
 
-	        for(int i = 0; i<delimiters.length(); ++i){
-	            d.add(delimiters.charAt(i));}
-	   
-	        int i = 0;
-	        int j = i+1;
-	        
-	        while(j<s.length()){
-	            if(d.contains(s.charAt(j))){
-	                String t = s.substring(i, j);
-	                display.add(t);
-	                i = j+1;
-	                ++j;}
-	            
-	            else{
-	                ++j;}}
-	        
-	        return display;}
-	   
-	    public static void main(String[] args) {
-	        System.out.println(split("AB#C D?EF#45", "# ?"));}
+		for (int i = 0; i < delimiters.length(); ++i) {
+			d.add(delimiters.charAt(i));
+		}
+
+		int i = 0;
+		int j = i + 1;
+
+		while (j < s.length()) {
+			if (d.contains(s.charAt(j))) {
+				String t = s.substring(i, j);
+				display.add(t);
+				i = j + 1;
+				++j;
+			}
+
+			else {
+				++j;
+			}
+		}
+
+		return display;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(split("AB#C D?EF#45", "# ?"));
+	}
 }
-
-
-
